7050c174f012c947897bd9d70ea2748ac8e0fa75
Rohan Moitra <rohanmoitra@gmail.com>
2013-02-02 19:07:29 -0600

    Finishing Layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "user/new"
 
+  get "static_pages/home"
+  match 'signup', to: 'user#new'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,17 +3,14 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit root_path
-      page.should have_content('Sample App')
-    end
+    before {visit root_path}
+      it "should have the content 'Sample App'" do
+        page.should have_content('Sample App')
+      end
     
-    it "should have the right title" do
-      visit root_path
-      page.should have_selector('title',:text => "Sample App | Home")
-    end
-
+      it "should have the right title" do
+        page.should have_selector('title',:text => "Sample App | Home")
+      end
   end
 
   describe "Help page" do
